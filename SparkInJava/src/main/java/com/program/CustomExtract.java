5177090b91e2c8446f393e2b6596f63cc53fade9
Santosh Nijalingappa <nnsantosh@gmail.com>
2017-08-02 01:13:21 -0700

    modify particular json attribute through custom function

--- SparkInJava/src/main/java/com/program/CustomExtract.java
+++ SparkInJava/src/main/java/com/program/CustomExtract.java
@@ -1,8 +1,13 @@
 package com.program;
 
+import static org.apache.spark.sql.functions.callUDF;
+import static org.apache.spark.sql.functions.col;
+
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
 
 public class CustomExtract {
 
@@ -18,6 +23,21 @@ public class CustomExtract {
 		Dataset<Row> outDs = spark.read().json("data/test2.json");
 		outDs.show();
 		outDs.printSchema();
+		
+		Dataset<Row> outDs2 = outDs.select("glossary");
+		outDs2.show();
+		outDs2.printSchema();
+		
+		Dataset<Row> outDs3 = outDs2.select("glossary.title");
+		
+		SQLContext sqlContext = spark.sqlContext();
+		
+		sqlContext.udf().register("udfUppercase",
+				  (String string) -> string.toUpperCase(), DataTypes.StringType);
+
+		Dataset<Row> modifiedDs = outDs3.select(callUDF("udfUppercase", col("title")));
+		modifiedDs.show();
+		modifiedDs.printSchema();
 		
 	}
 }
