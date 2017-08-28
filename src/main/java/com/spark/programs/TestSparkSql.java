a9ec36593e68185073e4dc004d77997ad01f8a47
D286920 <D286920@CNPTCAM5131856.ptc.ca.kp.org.>
2017-08-28 09:39:16 -0700

    updated programs

---
+++ src/main/java/com/spark/programs/TestSparkSql.java
@@ -0,0 +1,62 @@
+package com.spark.programs;
+
+//import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.api.java.JavaRDD;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.SparkConf;
+import static org.apache.spark.sql.functions.col;
+
+
+public class TestSparkSql {
+	
+	public static void main(String[] args) {
+		
+		
+		/*SparkConf conf = new SparkConf().setAppName("Line_Count").setMaster("local[2]");
+		JavaSparkContext ctx = new JavaSparkContext(conf);*/
+		/*JavaRDD<String> linesRDD = ctx.textFile("C:\\Users\\SantSavi\\Documents\\Git_Commands.txt");
+		System.out.println("WordCount.main() linesRDD count: "+linesRDD.count());*/
+		//List<String> jsonData = Arrays.asList(
+		        //"{\"name\":\"Yin\",\"address\":{\"city\":\"Columbus\",\"state\":\"Ohio\"}}");
+		
+
+	
+		
+		 /*SparkSession spark = SparkSession.builder().appName("Build a DataFrame from Scratch").master("local[2]")
+		            .getOrCreate();*/
+		 SparkSession spark = SparkSession
+			      .builder().master("local")
+			      .appName("Java Spark SQL data sources example")
+			      .config("spark.some.config.option", "some-value")
+			      .getOrCreate();
+		// A JSON dataset is pointed to by path.
+		// The path can be either a single text file or a directory storing text files.
+		Dataset<Row> outDs = spark.read().json("data/test.json");
+		outDs.show();
+		outDs.printSchema();
+		// Select only the "name" column
+		Dataset<Row> outDs1 = outDs;
+		Dataset<Row> outDs2 = outDs1.select("a");
+		outDs2.show();
+		Dataset<Row> outDs3 = outDs;
+		// Select all columns, but increment the b by 1
+		outDs3.select(col("a"), col("b").plus(1)).show();
+		Dataset<Row> outDs4 = outDs;
+		outDs4.filter(col("b").gt(3)).show();
+		
+		//ctx.close();
+		
+		//SparkContext
+		
+		
+		
+	}
+
+}
--- src/main/java/com/spark/programs/WordCount.java
+++ src/main/java/com/spark/programs/WordCount.java
@@ -1,62 +1,67 @@
 package com.spark.programs;
 
-//import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SQLContext;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.api.java.JavaRDD;
-
 import java.util.Arrays;
-import java.util.List;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.apache.spark.SparkConf;
-import static org.apache.spark.sql.functions.col;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
 
-
+import scala.Tuple2;
 public class WordCount {
 	
-	public static void main(String[] args) {
-		
-		
-		/*SparkConf conf = new SparkConf().setAppName("Line_Count").setMaster("local[2]");
-		JavaSparkContext ctx = new JavaSparkContext(conf);*/
-		/*JavaRDD<String> linesRDD = ctx.textFile("C:\\Users\\SantSavi\\Documents\\Git_Commands.txt");
-		System.out.println("WordCount.main() linesRDD count: "+linesRDD.count());*/
-		//List<String> jsonData = Arrays.asList(
-		        //"{\"name\":\"Yin\",\"address\":{\"city\":\"Columbus\",\"state\":\"Ohio\"}}");
-		
+	private static final FlatMapFunction<String, String> WORDS_EXTRACTOR = new FlatMapFunction<String, String>() {
+		private static final long serialVersionUID = 1L;
 
-	
-		
-		 /*SparkSession spark = SparkSession.builder().appName("Build a DataFrame from Scratch").master("local[2]")
-		            .getOrCreate();*/
-		 SparkSession spark = SparkSession
+		public Iterator<String> call(String s) throws Exception {
+			System.out.println("text file"+s);
+			return Arrays.asList(s.split(" ")).iterator();
+		}
+	};
+
+	private static final PairFunction<String, String, Integer> WORDS_MAPPER = new PairFunction<String, String, Integer>() {
+		private static final long serialVersionUID = 1L;
+
+		public Tuple2<String, Integer> call(String s) throws Exception {
+			return new Tuple2<String, Integer>(s, 1);
+		}
+	};
+
+	private static final Function2<Integer, Integer, Integer> WORDS_REDUCER = new Function2<Integer, Integer, Integer>() {
+		private static final long serialVersionUID = 1L;
+
+		public Integer call(Integer a, Integer b) throws Exception {
+			return a + b;
+		}
+	};
+
+	public static void main(String[] args) {
+
+		SparkSession spark = SparkSession
 			      .builder().master("local")
 			      .appName("Java Spark SQL data sources example")
 			      .config("spark.some.config.option", "some-value")
 			      .getOrCreate();
-		// A JSON dataset is pointed to by path.
-		// The path can be either a single text file or a directory storing text files.
-		Dataset<Row> outDs = spark.read().json("data/test.json");
-		outDs.show();
-		outDs.printSchema();
-		// Select only the "name" column
-		Dataset<Row> outDs1 = outDs;
-		Dataset<Row> outDs2 = outDs1.select("a");
-		outDs2.show();
-		Dataset<Row> outDs3 = outDs;
-		// Select all columns, but increment the b by 1
-		outDs3.select(col("a"), col("b").plus(1)).show();
-		Dataset<Row> outDs4 = outDs;
-		outDs4.filter(col("b").gt(3)).show();
-		
-		//ctx.close();
-		
-		//SparkContext
-		
-		
-		
-	}
 
+		
+		Dataset<String> file = spark.read().textFile("data/testForWordCount.txt");
+		JavaRDD<String> fileRDD = file.toJavaRDD();
+		
+		JavaRDD<String> words = fileRDD.flatMap(WORDS_EXTRACTOR);
+		JavaPairRDD<String, Integer> pairs = words.mapToPair(WORDS_MAPPER);
+		JavaPairRDD<String, Integer> counter = pairs.reduceByKey(WORDS_REDUCER);
+		counter.saveAsTextFile("data/WordCountOutput");
+		//counter.collect();
+		
+		//System.out.println("WordCount.main() counterMap: "+counterMap);
+
+	}
+	
 }
